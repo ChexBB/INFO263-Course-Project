9440fe1881143408a6fd4ce718a114711c008a0c
Chuan Law <cllaw_1997@hotmail.com>
2017-09-22 15:36:02 +1200

    Added map to index page.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once 'include/header.php';
 ?>
 
 <script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_API_KEY_HERE&callback=initMap">
+  src="https://maps.googleapis.com/maps/api/js?callback=initMap">
 </script>
 <script src="scripts/map.js"></script>
 <script>
@@ -12,6 +12,40 @@ require_once 'include/header.php';
 
     });
 </script>
+
+<html>
+    <head>
+    <style>
+       #map {
+        height: 80%;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>GoBus - Aucklands best transport app</h3>
+    <div id="map"></div>
+    <script>
+      function initMap() {
+        var uluru = {lat: -36.849316, lng: 174.766249};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 12,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?callback=initMap">
+    </script>
+  </body>
+
+</html>
+
 <?php
 require_once 'include/footer.php';
 ?>
+
