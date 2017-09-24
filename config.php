289ebd5873a4dfe7703c64afd5e8590c9bddf0b3
ChexBB <30458769+ChexBB@users.noreply.github.com>
2017-09-24 18:20:31 +1300

    Rearranged stuff

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php //config.php
-$hostname = "csse-info263.canterbury.ac.nz";
-$database = "akl_transport";
-$username = "info263";
-$password = "info263";
-$APIKey = ""; # Your API Key here.
-?>
