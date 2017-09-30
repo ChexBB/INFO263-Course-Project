5367714d813f7a765ea56d907a4ca8354aa77845
ChexBB <30458769+ChexBB@users.noreply.github.com>
2017-09-30 18:15:56 +1300

    Moved map initiation to map.js
    initMap function located in map.js and called from index.php
    
    renamed scripts folder to script as it included the <script> tags in every file. Not sure if its okay to do it like this? Can revert later.

--- index.php
+++ index.php
@@ -4,45 +4,31 @@ require_once 'include/header.php';
 ?>
 
 <script async defer
-  src="https://maps.googleapis.com/maps/api/js?callback=initMap">
+  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBajjU1EoGpVz0QKgSL2c_aS6vJDb8N5cA&callback=initMap">
 </script>
-<script src="scripts/map.js"></script>
+<script src="script/map.js"></script>
+
 <script>
     $(document).ready(function() {
-
+		
     });
 </script>
 
 <html>
-    <head>
+  <head>
     <style>
        #map {
-        height: 80%;
+        height: 800px;
         width: 100%;
        }
     </style>
   </head>
   <body>
-    <h3>GoBus - Aucklands best transport app</h3>
+    <h3>GoBus Transport App</h3>
     <div id="map"></div>
-    <script>
-      function initMap() {
-        var uluru = {lat: -36.849316, lng: 174.766249};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 12,
-          center: uluru
-        });
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?callback=initMap">
-    </script>
+   
+    
   </body>
-
 </html>
 
 <?php
---
+++ script/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+	var auckland = {lat: -36.849316, lng: 174.766249};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 12,
+	  center: auckland
+	});
+	var marker = new google.maps.Marker({
+	  position: auckland,
+	  map: map
+	});
+}
