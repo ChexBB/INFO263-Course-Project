e6bd87fd43dd173cdc554c6d698d5d0839b20624
ChexBB <30458769+ChexBB@users.noreply.github.com>
2017-10-06 23:01:45 +1300

    Fixed the bad merge sorry

--- index.php
+++ index.php
@@ -5,14 +5,6 @@ require_once 'database.php';
 
 ?>
 
-<<<<<<< HEAD
-=======
-<script async defer
-  src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBajjU1EoGpVz0QKgSL2c_aS6vJDb8N5cA&callback=initMap">
-</script>
-<script src="script/map.js"></script>
-
->>>>>>> 5367714d813f7a765ea56d907a4ca8354aa77845
 <script>
     $(document).ready(function() {
 		
@@ -21,32 +13,16 @@ require_once 'database.php';
 
 <html>
   <head>
-<<<<<<< HEAD
 	<link rel="stylesheet" href="CSS/master.css" type="text/css">
   
 	<h1>GoBus Transport App</h1>
   
 	<p id="selectRoute">Select a Route:</p>
     
-=======
-    <style>
-       #map {
-        height: 800px;
-        width: 100%;
-       }
-    </style>
->>>>>>> 5367714d813f7a765ea56d907a4ca8354aa77845
   </head>
   
   <body>
-<<<<<<< HEAD
 
-=======
-    <h3>GoBus Transport App</h3>
-    <div id="map"></div>
-   
-    
->>>>>>> 5367714d813f7a765ea56d907a4ca8354aa77845
   </body>
 </html>
 
@@ -60,4 +36,3 @@ require_once 'database.php';
 <?php
 require_once 'include/footer.php';
 ?>
-
