3ae4862139e3f78c40b39e8bcd8030286f47267e
GEOFAIRY <kst84@uclive.ac.nz>
2017-10-11 16:58:26 +1300

    changed api key

--- include/config.php
+++ include/config.php
@@ -3,5 +3,5 @@ $hostname = "csse-info263.canterbury.ac.nz";
 $database = "akl_transport";
 $username = "info263";
 $password = "info263";
-$APIKey = "31075fd30ae54c7ab171385fbea7d8d4"; # Your API Key here.
+$APIKey = "a9ca74f41dc3423aaad97325c9a86d37"; # Your API Key here.
 ?>
