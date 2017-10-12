005f17ad0e0e07c6b033b9c9d291296316686f74
Chuan <cll62@uclive.ac.nz>
2017-10-12 21:45:23 +1300

    Deleting duplicate file

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php //config.php
-$hostname = "csse-info263.canterbury.ac.nz";
-$database = "akl_transport";
-$username = "info263";
-$password = "info263";
-$APIKey = "31075fd30ae54c7ab171385fbea7d8d4"; # Your API Key here. Chuan's Key.
-?>
