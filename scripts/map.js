9e916f03cec062b2fc31e5909084c07d93027e27
GEOFAIRY <kst84@uclive.ac.nz>
2017-10-16 00:01:41 +1300

    Added timed function calls, excluded default route from function and added map zeroing

--- scripts/map.js
+++ scripts/map.js
@@ -29,59 +29,48 @@ function deleteMarkers() {
 
 function apiQuery() {
 	var query_route = $('#route_picker').val();
-	var response;
-	var ajaxRequest = new XMLHttpRequest();
-	var vehicle_array;
-	ajaxRequest.onreadystatechange = function(){
-    	if(ajaxRequest.readyState == 4){
-    		response = document.getElementsByTagName('body');
-    		response.innerHTML = ajaxRequest.responseText;
-    		alert(response.innerHTML);
-			vehicle_array = response.innerHTML.split("},{");
-			vehicle_array[0] = vehicle_array[0].substr(2);
-			vehicle_array[vehicle_array.length-1] = vehicle_array[vehicle_array.length-1].slice(0, (vehicle_array[vehicle_array.length-1].length-2));
-			alert(vehicle_array[0]);
-			alert(vehicle_array[vehicle_array.length-1]);
-			alert(vehicle_array.length);
-			deleteMarkers();
-			var lngN;
-			var commaPos;
-			var latN;
-			var latLng;
-			var marker;
-			for (var i = 0; i <= vehicle_array.length-1; i++) {
-				firstSemi = vehicle_array[i].indexOf(':');
-				lngN = vehicle_array[i].indexOf(':',firstSemi+1)+1;
-				commaPos = vehicle_array[i].indexOf(',', lngN+1)+1;
-				latN = vehicle_array[i].indexOf(':',lngN+1)+1;
-				latLng = {lat: parseFloat(vehicle_array[i].slice(latN, vehicle_array[i].length-1)), lng: parseFloat(vehicle_array[i].slice(lngN, commaPos))};
-				marker = new google.maps.Marker({
-					position: latLng,
-					map: map
-				});
-				markers.push(marker);
+	if (query_route != "set") {
+		var response;
+		var ajaxRequest = new XMLHttpRequest();
+		var vehicle_array;
+		ajaxRequest.onreadystatechange = function(){
+			if(ajaxRequest.readyState == 4){
+				response = document.getElementsByTagName('body');
+				response.innerHTML = ajaxRequest.responseText;
+				alert(response.innerHTML);
+				vehicle_array = response.innerHTML.split("},{");
+				vehicle_array[0] = vehicle_array[0].substr(2);
+				vehicle_array[vehicle_array.length-1] = vehicle_array[vehicle_array.length-1].slice(0, (vehicle_array[vehicle_array.length-1].length-2));
+				alert(vehicle_array[0]);
+				alert(vehicle_array[vehicle_array.length-1]);
+				alert(vehicle_array.length);
+				deleteMarkers();
+				var lngN;
+				var commaPos;
+				var latN;
+				var latLng;
+				var marker;
+				for (var i = 0; i <= vehicle_array.length-1; i++) {
+					firstSemi = vehicle_array[i].indexOf(':');
+					lngN = vehicle_array[i].indexOf(':',firstSemi+1)+1;
+					commaPos = vehicle_array[i].indexOf(',', lngN+1)+1;
+					latN = vehicle_array[i].indexOf(':',lngN+1)+1;
+					latLng = {lat: parseFloat(vehicle_array[i].slice(latN, vehicle_array[i].length-1)), lng: parseFloat(vehicle_array[i].slice(lngN, commaPos))};
+					marker = new google.maps.Marker({
+						position: latLng,
+						map: map
+					});
+					map.panTo(latLng);
+					map.setZoom(11)
+					markers.push(marker);
+				}
 			}
-   		}
-   	}
-    ajaxRequest.open("GET", "vehicle_query.php?r=" + query_route, true);
-    ajaxRequest.send();
-	
+		}
+		ajaxRequest.open("GET", "vehicle_query.php?r=" + query_route, true);
+		ajaxRequest.send();
+	}
 }
 
-//function to add markers for each bus
-//@param busLocations array containing all the buses to be displayed
-//need to call from processJSON somehow? 
-/**function showVehicles(busLocation) {
-    var bounds = new google.maps.LatLngBounds();
-
-    //removes of all existing markers
-    markers.forEach(function (clear) {
-        clear.setMap(null);
-    });
-
-    busLocation.forEach(function (bus) {
-        var busMarker = new google.maps.Marker({
-            position: { lat: bus.latitude, lng: bus.longitude },
-            title: 'Bus: ' + bus.vehicle_id, map: map;
-        }); **/
+setInterval(function(){
+	apiQuery()}, 30000);
        
